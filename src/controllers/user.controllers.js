bd3962739c28fce4eec655a0808335218dc019fa
Faique <faique.official695@gmail.com>
2025-07-23 09:15:53 +0530

    feat: Implement user authentication with JWT and add login/logout functionality

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -7,6 +7,27 @@ import jwt from 'jsonwebtoken';
 // import { use } from 'react';
 
 
+const generateAccessAndRefreshTokens = async(userId) =>
+{
+  try{
+   const user =  await User.findById(userId);
+   const accessToken = user.generateAccessToken();
+   const refreshToken = user.generateRefreshToken();
+
+   user.refreshToken = refreshToken;
+     await  user.save({validateBeforeSave: false});
+
+     return {accessToken, refreshToken};
+
+
+  }
+  catch(error) {
+ 
+    throw new ApiError(500, "Internal server error");
+
+
+  }
+}
 
 
 
@@ -22,6 +43,8 @@ const registerUser = asyncHandler(async (req, res) => {
   //return response
 
   const {fullName,email , username,password}  = req.body
+  console.log(req.files);
+  
   console.log("Yeah h naam", fullName);
   console.log("Yeah h email", email);
   console.log("Yeah h password", password);
@@ -85,9 +108,86 @@ const registerUser = asyncHandler(async (req, res) => {
    }
 
    return res.status(201).json(
-    new ApiResponse(200, user, "User registered successfully"))
+    new ApiResponse(200, createdUser, "User registered successfully"))
 
 
+
+
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+
+  //req body se data lelo
+  // username se ya email se user ko dhoondo
+  //find user
+  //agar user nahi mila toh error throw karo
+  //agar user mila toh password check karo
+  //agar password match nahi hua toh error throw karo
+  //agar password match hua toh access and refresh token generate karo and user ko bhejo
+  //send cookies with tokens
+
+  const { email ,  username, password } = req.body
+  if(!username && !email) {
+    throw new ApiError(400, "Username ya email me se ek toh chahiye bhai");
+  }
+
+   const user = await User.findOne({$or: [{username}, {email}]})
+
+  if(!user) {
+    throw new ApiError(404, "User not found");
+  }
+ const isPasswordValid =  await user.isPasswordCorrect(password)
+
+ if(!isPasswordValid) {
+    throw new ApiError(401, "Invalid password");
+  }
+
+      const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+      const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+      const options = {
+        httpOnly: true,
+        secure: true,
+        };
+
+        return res.status(200).cookie("accessToken", accessToken, options)
+                              .cookie("refreshToken", refreshToken, options)
+                              .json(new ApiResponse(200,
+                                {
+                                  user: loggedInUser,
+                                  accessToken,
+                                  refreshToken
+                                }, "User logged in successfully"
+                                ));
+
+
+
+  
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    
+   await User.findByIdAndUpdate(
+      req.user._id,
+      {
+      $set:{refreshToken: undefined}
+      },
+       {
+        new:true,
+       } 
+      )
+
+         const options = {
+        httpOnly: true,
+        secure: true,
+        }
+
+        return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged out successfully"));
 
 
 });
@@ -96,7 +196,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
 
-export {registerUser}; 
+export {registerUser,loginUser,logoutUser}; 
 
 
 
